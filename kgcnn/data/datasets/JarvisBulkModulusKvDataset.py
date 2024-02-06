d10bdc680923ac56f5ed1ae009bda1b8a7b2b626
HW DU <44920119+dhw059@users.noreply.github.com>
2024-02-06 15:08:20 +0800

    Add files via upload

---
+++ kgcnn/data/datasets/JarvisBulkModulusKvDataset.py
@@ -0,0 +1,28 @@
+from kgcnn.data.datasets.JarvisBenchDataset2021 import JarvisBenchDataset2021
+
+
+class JarvisBulkModulusKvDataset(JarvisBenchDataset2021):
+    """Store and process :obj:`MatProjectJdft2dDataset` from `MatBench <https://matbench.materialsproject.org/>`__
+    database. Name within Matbench: 'matbench_jdft2d'.
+
+    Matbench test dataset for predicting exfoliation energies from crystal structure
+    (computed with the OptB88vdW and TBmBJ functionals). Adapted from the JARVIS DFT database.
+    For benchmarking w/ nested cross validation, the order of the dataset must be identical to the retrieved data;
+    refer to the Automatminer/Matbench publication for more details.
+
+        * Number of samples: 636
+        * Task type: regression
+        * Input type: structure
+
+    """
+
+    def __init__(self, reload=False, verbose: int = 10):
+        r"""Initialize 'bulk_modulus_kv' dataset.
+
+        Args:
+            reload (bool): Whether to reload the data and make new dataset. Default is False.
+            verbose (int): Print progress or info for processing where 60=silent. Default is 10.
+        """
+        super(JarvisBulkModulusKvDataset, self).__init__("bulk_modulus_kv", reload=reload, verbose=verbose)
+        self.label_names = "bulk_modulus_kv "
+        self.label_units = "Gpa"
