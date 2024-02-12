767d18c62f88d8e9b4805d2dd46c824d23e8ec60
HW DU <44920119+dhw059@users.noreply.github.com>
2024-02-12 15:40:02 +0800

    Add files via upload

---
+++ kgcnn/data/datasets/JarvisEhullDataset.py
@@ -0,0 +1,31 @@
+from kgcnn.data.datasets.JarvisBenchDataset2021 import JarvisBenchDataset2021
+
+
+class JarvisEhullDataset(JarvisBenchDataset2021):
+    """Store and process :obj:`MatProjectJdft2dDataset` from `MatBench <https://matbench.materialsproject.org/>`__
+    database. Name within Matbench: 'matbench_jdft2d'.
+
+    Matbench test dataset for predicting exfoliation energies from crystal structure
+    (computed with the OptB88vdW and TBmBJ functionals). Adapted from the JARVIS DFT database.
+    For benchmarking w/ nested cross validation, the order of the dataset must be identical to the retrieved data;
+    refer to the Automatminer/Matbench publication for more details.
+
+        * n_train: 44290
+        * n_val: 5537
+        * n_test: 5537
+        * Task type: regression
+        * Input type: structure
+
+    """
+
+    def __init__(self, reload=False, verbose: int = 10):
+        r"""Initialize 'ehull' dataset.
+
+        Args:
+            reload (bool): Whether to reload the data and make new dataset. Default is False.
+            verbose (int): Print progress or info for processing where 60=silent. Default is 10.
+        """
+        super(JarvisEhullDataset, self).__init__("ehull", reload=reload, verbose=verbose)
+        self.label_names = "ehull "
+        self.label_units = "No unit"
+
